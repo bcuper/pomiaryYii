77457f7525b62e9012a7bc0409f69787e147fddc
Bartek Cuper <bartek.cuper@gmail.com>
2022-11-03 11:33:32 +0000

    tcw + rosliny

---
+++ views/rosliny/parapet.php
@@ -0,0 +1,16 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap5\LinkPager;
+
+$this->title = 'Parapet';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="pomiary-index">
+
+    <h1>
+        Rośliny Parapet
+    </h1>
+
+    
+    </div>
+        
---
+++ views/rosliny/regal.php
@@ -0,0 +1,16 @@
+<?php
+use yii\helpers\Html;
+use yii\bootstrap5\LinkPager;
+
+$this->title = 'Regal';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="pomiary-index">
+
+    <h1>
+        Rośliny Regał
+    </h1>
+
+    
+    </div>
+        
